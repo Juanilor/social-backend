e9b05d978de9f01413b4d56b9447b66175f539a2
Juanilor <LordiJuan@hotmail.com>
2026-05-04 16:47:45 -0300

    Creada funcion getMe en Auth Service, esta funcion me devuelve el usuario actual, si es que coincide con el token a travez del middleware de Auth Middleware. Esto me va a servir para no tener que volver a hacer login cada vez que el usuario realice una accion

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,22 @@ import { Request, Response } from "express";
 
 import { registerUser, loginUser } from "../services/auth.service";
 
+import User from "../models/User";
+
+import { AuthRequest } from "../middlewares/auth.middleware";
+
+
+export const getMe = async (req: AuthRequest, res: Response) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Error de servidor" });
+    }
+};
+
+
 export const register = async (req: Request, res: Response) => {
     try {
         const { username, email, password } = req.body;
@@ -25,3 +41,4 @@ export const login = async (req: Request, res: Response) => {
         res.status(400).json({ message: error.message })
     }
 }
+
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 
 import { register, login } from "../controllers/auth.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { getMe } from "../controllers/auth.controller";
+
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/me", authMiddleware, getMe);
 
 export default router;
