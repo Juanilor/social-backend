353f51f3d51ee8b04b041a6ed732aa6a478c0f51
Juanilor <LordiJuan@hotmail.com>
2026-05-07 11:36:20 -0300

    Implementado Validator Middleware en rutas Auth.

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import {validate} from "../middlewares/validation.middleware"
+
 
 import { register, login } from "../controllers/auth.controller";
 import { authMiddleware } from "../middlewares/auth.middleware";
@@ -7,8 +10,18 @@ import { getMe } from "../controllers/auth.controller";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register",[
+
+    body("username").notEmpty().withMessage("Username requerido."),
+    body("email").isEmail().withMessage("Email invalido."),
+    body("password").isLength({min: 6}).withMessage("Minimo 6 caracteres."),
+], validate , register);
+router.post("/login", [
+
+    body("email").isEmail().withMessage("Email invalido"),
+    body("password").isLength({min: 6}).withMessage("Minimo 6 caracteres.")
+
+], validate, login);
 router.get("/me", authMiddleware, getMe);
 
 export default router;
