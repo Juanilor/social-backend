f73aa62e923028278945016e1f621e3cc889ac1a
Juanilor <LordiJuan@hotmail.com>
2026-05-07 09:10:34 -0300

    Creado Model/Schema de Post

---
+++ src/models/Post.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import { timeStamp } from "node:console";
+
+export interface IPost extends Document {
+
+    content: string;
+    author: mongoose.Types.ObjectId;
+    likes: mongoose.Types.ObjectId[];
+    createAt: Date;
+
+}
+
+const PostSchema = new Schema<IPost>(
+    {
+        content: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        author: {
+            type: mongoose.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        likes: [{
+            type: mongoose.Types.ObjectId,
+            ref: "User"
+        }]
+    },
+    { timestamps: true }
+);
+
+
+export default mongoose.model<IPost>("Post", PostSchema);
