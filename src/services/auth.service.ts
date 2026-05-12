e9045ff498f78fe6c959cba26e5c8297135dd69f
Juanilor <LordiJuan@hotmail.com>
2026-05-12 11:43:56 -0300

    Mejorado test de register, y agregado test para login. Acomode tambien lugares que me generaban errores a la hora de hacer el test

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,6 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { error } from "node:console";
 import { AppError } from "../utils/AppError";
 
 export const registerUser = async (
@@ -23,30 +22,35 @@ export const registerUser = async (
         password: hashedPassword,
     });
 
+    const token = jwt.sign(
+        { id: user._id },
+        process.env.JWT_SECRET as string, { expiresIn: '7d' }
+    )
+
     const userObj = user.toObject();
 
-    const {password:  _password, ...userWithoutPassword} = userObj; 
-    
-    return userWithoutPassword;
+    const { password: _password, ...userWithoutPassword } = userObj;
+
+    return {user: userWithoutPassword, token};
 }
 
 export const loginUser = async (email: string, password: string) => {
     const user = await User.findOne({ email });
-    
+
     if (!user) {
-        throw new AppError("El usuario no existe", 404);
+        throw new AppError("El usuario no existe", 401);
     }
-    
+
     const isMatch = await bcrypt.compare(password, user.password);
-    
+
     if (!isMatch) {
         throw new AppError("Credenciales invalidas", 401);
     }
-    
+
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET as string, { expiresIn: "1d" });
-    
+
     const userObj = user.toObject();
-    const {password:  _password, ...userWithoutPassword} = userObj; 
+    const { password: _password, ...userWithoutPassword } = userObj;
 
 
     return { user: userWithoutPassword, token }
--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -5,13 +5,59 @@ import app from "../app";
 describe("Auth Routes", () => {
     it("Should register a new user", async () => {
         const response = await request(app)
-        .post("/api/auth/register")
-        .send({
-            username: `testUser${Date.now()}`, 
-            email: `test${Date.now()}@test.com`, 
-            password: "123456",
-        });
+            .post("/api/auth/register")
+            .send({
+                username: `testUser${Date.now()}`,
+                email: `test${Date.now()}@test.com`,
+                password: "123456",
+            });
 
         expect(response.status).toBe(201);
+
+        expect(response.body).toHaveProperty("token");
+
+        expect(response.body.user).toHaveProperty("_id");
+
+        expect(response.body.user.email).toContain("@test.com");
+
+
     }, 10000);
+
+    it("Should login user", async () => {
+        const email = `test${Date.now()}@test.com`;
+
+        await request(app)
+            .post("/api/auth/register")
+            .send({
+                username: "testUser",
+                email,
+                password: "123456",
+            });
+
+        const response = await request(app).post("/api/auth/login").send({ email, password: '123456', });
+
+        expect(response.status).toBe(200);
+
+        expect(response.body).toHaveProperty("token");
+
+
+
+
+
+    }, 10000)
+
+
+    it("Should fail with invalid credentials", async () => {
+
+        const response = await request(app)
+            .post("/api/auth/login")
+            .send({
+                email: "wrong@test.com",
+                password: '123456',
+            });
+
+        expect(response.status).toBe(401);
+
+    }, 10000)
 });
+
